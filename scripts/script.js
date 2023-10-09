e6f8744cd9f0a19600e3c1d8130e3ce113ffe3e4
Daksh <dakshkaul7790@gmail.com>
2023-10-09 17:58:15 +0530

    Added Information to almost all of the html, excluding the skills disc. Added Black&white styling to most of webpage. added imsg fake status bar timer script.

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+function updateTime() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var meridiem = "AM"; // Default to AM
+
+    // Convert to 12-hour format and determine AM/PM
+    if (hours > 12) {
+        hours = hours - 12;
+        meridiem = "PM";
+    }
+    if (hours === 0) {
+        hours = 12;
+    }
+
+    // Pad single digit minutes with leading zero
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+
+    // Format the time as HH:MM AM/PM
+    var formattedTime = hours + ":" + minutes + " " + meridiem;
+
+    // Update the content of the h1 element with the current time
+    document.getElementById("currentTime").textContent = formattedTime;
+}
+
+// Call updateTime function once on page startup
+updateTime();
+
+// Call updateTime function every minute (60,000 milliseconds)
+setInterval(updateTime, 60000);
+
